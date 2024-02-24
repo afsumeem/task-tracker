cb41f6e9373fd1f60e6ffb22d0390b10a9cc0532
Afsana Meem <afsumeem@gmail.com>
2024-02-24 21:42:07 +0600

    home page done

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,13 +1,22 @@
 import TodoForm from "@/components/TodoForm";
 import TodoLists from "@/components/TodoLists";
 import React from "react";
+import styles from "../styles/Todo.module.css";
 
 const TodoPage = () => {
   return (
-    <div>
-      <TodoForm />
-      <TodoLists />
-    </div>
+    <main
+      className={`${styles.todoContainer} d-flex justify-content-center align-items-center`}
+    >
+      <div className={`${styles.heroSection} px-4 rounded-3`}>
+        {/* my todo container */}
+        <h3 className="fw-bold text-center mt-4">MY TODO</h3>
+        <div>
+          <TodoForm />
+          <TodoLists />
+        </div>
+      </div>
+    </main>
   );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { FaArrowRight } from "react-icons/fa";
+import { Button } from "react-bootstrap";
+import { useRouter } from "next/router";
 import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
@@ -14,7 +13,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main
+        className={`${styles.todoContainer} d-flex justify-content-center align-items-center`}
+      >
+        <div className={`${styles.heroSection}`}>
+          {/* my todo title */}
+          <h3 className="fw-bold text-center mt-4">MY TODO</h3>
+
+          {/* banner image */}
+          <Image
+            src="/banner.svg"
+            alt="homePageBanner"
+            className="d-block m-auto"
+            width={200}
+            height={200}
+          />
+
+          {/* get started button */}
+          <div className={`${styles.startTaskContainer} d-block m-auto p-4`}>
+            <p className="text-center fs-6">
+              This Todo App provides you to manage your tasks effectively.
+            </p>
+            <p className="text-center fs-5 mb-4">
+              Start managing your tasks today!
+            </p>
+          </div>
+          <Button onClick={handleClick} className={styles.getStartedBtn}>
+            <FaArrowRight />
+          </Button>
+        </div>
+      </main>
     </>
   );
 }
